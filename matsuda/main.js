f4c6e53bfc0484b8c22a7e1207151e391dab8f02
smcon <165791594+smcon@users.noreply.github.com>
2024-08-31 19:46:25 +0900

    Add files via upload

--- matsuda/main.js
+++ matsuda/main.js
@@ -18,11 +18,26 @@ for (let i = 0; i < linkElem.length; i++) {
         stalker.classList.remove('is_active');
     });
 }
-
+/*スリックスライダー */
 $('.slider').slick({
-  autoplay:true, // 自動再生を設定
-  autoplaySpeed:1500, // スライド切り替えの時間を設定
-  dots:true // インジケーターを表示
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 5000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 4, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
+        }
+      }
+    ]
+
 });
 
 $(window).scroll(function (){
